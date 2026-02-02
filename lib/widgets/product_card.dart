0381af057c4dc4ff495c8522f4d045e826257639
FranciscoSousa25 <franciscowsousa22@gmail.com>
2026-02-02 20:01:48 -0300

    Subindo Widget

---
+++ lib/widgets/product_card.dart
@@ -0,0 +1,74 @@
+﻿import "package:flutter/material.dart";
+
+class ProductCard extends StatelessWidget {
+  final String image;
+  final String name;
+  final String price;
+
+  const ProductCard({
+    super.key,
+    required this.image,
+    required this.name,
+    required this.price,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 230,
+      margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: const [
+          BoxShadow(
+            color: Color.fromRGBO(0, 0, 0, 0.16),
+            offset: Offset(0, 1),
+            blurRadius: 4,
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start, // alinha à esquerda
+        children: [
+          // FOTO
+          ClipRRect(
+            borderRadius: BorderRadius.circular(12),
+            child: Image.asset(
+              image,
+              width: double.infinity,
+              height: 140,
+              fit: BoxFit.cover,
+            ),
+          ),
+
+          const SizedBox(height: 12),
+
+          // NOME
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            child: Text(
+              name,
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+            ),
+          ),
+
+          const SizedBox(height: 6),
+
+          // PREÇO
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+            child: Text(
+              price,
+              style: const TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+                color: Colors.green,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
