a7196f76d3faab035f344e9f5dad92193f9dd2d1
FranciscoSousa25 <franciscowsousa22@gmail.com>
2026-02-03 15:37:29 -0300

    subindo data produto

---
+++ lib/data/produtos_data.dart
@@ -0,0 +1,116 @@
+final List<Map<String, String>> lanches = [
+  {
+    'image': 'assets/imagens/humburger_simples.png',
+    'name': 'Hambúrguer Clássico',
+    'price': 'R\$ 24,90',
+  },
+  {
+    'image': 'assets/imagens/humburger_bacon.png',
+    'name': 'Hambúrguer com Bacon',
+    'price': 'R\$ 29,90',
+  },
+  {
+    'image': 'assets/imagens/humburger_duplo.png',
+    'name': 'Hambúrguer Duplo',
+    'price': 'R\$ 32,00',
+  },
+  {
+    'image': 'assets/imagens/frango_empanado.png',
+    'name': 'Frango Empanado',
+    'price': 'R\$ 21,00',
+  },
+  {
+    'image': 'assets/imagens/hot_dog.png',
+    'name': 'Hot Dog Especial',
+    'price': 'R\$ 18,50',
+  },
+  {
+    'image': 'assets/imagens/humburger_simples.png',
+    'name': 'X-Salada',
+    'price': 'R\$ 26,00',
+  },
+];
+
+final List<Map<String, String>> acompanhamentos = [
+  {
+    'image': 'assets/imagens/batata.png',
+    'name': 'Batata Frita',
+    'price': 'R\$ 12,00',
+  },
+  {
+    'image': 'assets/imagens/onion_rings.png',
+    'name': 'Onion Rings',
+    'price': 'R\$ 14,00',
+  },
+  {'image': 'assets/imagens/pudim.png', 'name': 'Pudim', 'price': 'R\$ 9,50'},
+  {
+    'image': 'assets/imagens/torta_de_limao.png',
+    'name': 'Torta de Limão',
+    'price': 'R\$ 15,50',
+  },
+  {
+    'image': 'assets/imagens/batata.png',
+    'name': 'Batata Cheddar',
+    'price': 'R\$ 16,00',
+  },
+  {
+    'image': 'assets/imagens/onion_rings.png',
+    'name': 'Onion Extra',
+    'price': 'R\$ 18,00',
+  },
+];
+
+final List<Map<String, String>> saudaveis = [
+  {
+    'image': 'assets/imagens/salada_saudavel.png',
+    'name': 'Salada Completa',
+    'price': 'R\$ 22,00',
+  },
+  {
+    'image': 'assets/imagens/mix_frutas.png',
+    'name': 'Mix de Frutas',
+    'price': 'R\$ 14,00',
+  },
+];
+
+final List<Map<String, String>> sobremesas = [
+  {'image': 'assets/imagens/pudim.png', 'name': 'Pudim', 'price': 'R\$ 9,50'},
+  {
+    'image': 'assets/imagens/torta_de_limao.png',
+    'name': 'Torta de Limão',
+    'price': 'R\$ 15,50',
+  },
+  {
+    'image': 'assets/imagens/brownie_sorvete.jpg',
+    'name': 'Brownie com Sorvete',
+    'price': 'R\$ 17,00',
+  },
+];
+
+final List<Map<String, String>> bebidas = [
+  {
+    'image': 'assets/imagens/suco_laranja.png',
+    'name': 'Suco Laranja',
+    'price': 'R\$ 6,00',
+  },
+  {
+    'image': 'assets/imagens/suco_limao.png',
+    'name': 'Suco Limão',
+    'price': 'R\$ 9,00',
+  },
+  {
+    'image': 'assets/imagens/fanta_laranja.png',
+    'name': 'Fanta Laranja',
+    'price': 'R\$ 4,00',
+  },
+  {
+    'image': 'assets/imagens/fanta_uva.png',
+    'name': 'Fanta Uva',
+    'price': 'R\$ 4,00',
+  },
+  {
+    'image': 'assets/imagens/coca_cola.png',
+    'name': 'Coca Cola',
+    'price': 'R\$ 4,00',
+  },
+];
